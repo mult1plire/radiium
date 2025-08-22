7b494549da584278ebc29ef2ddd8364d6af2dbd8
mult1plire <schoolmail.karson@gmail.com>
2025-08-22 11:22:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -43,3 +43,14 @@ app.get('/search', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+const path = require('path');
+
+// serve static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch-all 404 handler
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
